34b6fba0a978556cb0bd659cbe9aaec268d6fc42
mikm7243 <mikm7243@naver.com>
2023-11-25 23:09:55 +0900

    S4 11047 동전0 S5 7568 덩치 S5 1193 분수찾기 11/25

---
+++ src/implement/Fractions1193.java
@@ -0,0 +1,29 @@
+package implement;
+
+import java.util.Scanner;
+
+public class Fractions1193 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int remainder = sc.nextInt();
+        int count = 1;
+        int numerator = 0, denominator = 0;
+
+        //몇번째 지그재그 몇번째 자리인지 확인
+        while(remainder - count > 0){
+            remainder -= count;
+            count++;
+        }
+        //짝수 배치일경우
+        if(count % 2 == 0){
+            numerator =  1 + (remainder-1);
+            denominator = count - (remainder-1);
+        }
+        // 홀수 배치의 경우
+        else{
+            numerator = count - (remainder-1);
+            denominator = 1 + (remainder-1);
+        }
+        System.out.println(String.valueOf(numerator+"/"+denominator));
+    }
+}
