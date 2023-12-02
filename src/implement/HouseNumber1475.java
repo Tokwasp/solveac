c4e9676d47550e08d76e5cbc78c9aff5ca7aeb28
mikm7243 <mikm7243@naver.com>
2023-12-02 11:39:35 +0900

    S5 집합11723 S5 재귀17478

--- src/implement/HouseNumber1475.java
+++ src/implement/HouseNumber1475.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+/*
+   풀이 :
+   6을 9로 바꾸어서 풀었다.
+   배열을 돌며 9가 없다면 0~9 배열을 넣고 6을 다시 9로 바꿔주었다.
+ */
 public class HouseNumber1475 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] StringInput =br.readLine().split("");
+        StringBuilder sb = new StringBuilder();
         int[] input = Arrays.stream(StringInput).mapToInt(Integer::parseInt).toArray();
 
         ArrayList N = new ArrayList();
@@ -18,18 +22,17 @@ public class HouseNumber1475 {
 
         Collections.replaceAll(N,6,9);
         List list = new ArrayList();
-        List pack = new ArrayList();
+        Integer[] pack = new Integer[]{0,1,2,3,4,5,9,7,8,9};
         int count = 0;
 
         for(int i=0; i<N.size(); i++){
             if(!list.contains(N.get(i))){
-                pack = Stream.iterate(0, n -> n +1 ).limit(10).collect(Collectors.toList());
-                list.addAll(pack);
-                Collections.replaceAll(list,6,9);
+                Collections.addAll(list,pack);
                 count++;
             }
             list.remove(N.get(i));
+
         }
-        System.out.println(count);
+        System.out.println(sb.append(count));
     }
 }
