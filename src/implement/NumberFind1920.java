b1b8c7449589ce60fbf17c848026987e60a87413
mikm7243 <mikm7243@naver.com>
2024-03-28 17:53:15 +0900

    refactor: S4 수 찾기1920
    이진 탐색 반복호출이 재귀호출보다 성능이 좋아 변경

--- src/implement/NumberFind1920.java
+++ src/implement/NumberFind1920.java
@@ -4,48 +4,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 /*
-    문제 풀이법: 못풀어서 질문게시판을 통해 이분탐색을 써야한다는 것을 알았다.
-
-    공부해야할것 : 스트림 전환, 이분탐색
-    내림차순 정렬 : Collections.reverseOrder()
+    재 풀이(당일) :
+    첫째줄에 받은 배열을 정렬한후에 이분탐색을 사용하여 두번째받은 배열에 있는 수인지 체크한다.
+    이분탐색은 재귀와 반복호출이 있는데 성능적으로는 반복호출이 좋으나 가독성적으론 재귀가 더 좋다.
 */
 public class NumberFind1920 {
     static int[] arr;
 
-    //이진탐색
-    private static int binarySearch(int key, int low, int high) {
-        int mid;
-        mid = (low + high) / 2;
+    private static int getSolution(int findNum, int low, int high){
+        int mid = 0;
 
-        if (low <= high) {
-            if (key == arr[mid]) return 1;
-            else if (key > arr[mid]) return binarySearch(key, mid + 1, high);
-            else return binarySearch(key, low, mid - 1);
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(findNum == arr[mid]) return 1;
+            else if(findNum < arr[mid]) high = mid - 1;
+            else low = mid + 1;
         }
         return 0;
     }
 
     public static void main(String[] args) throws IOException {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        arr = new int[n];
+//
+//        for(int i=0; i<n; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        int m = sc.nextInt();
+//        int[] findArr = new int[m];
+//        for(int i=0; i<m; i++){
+//            findArr[i] = sc.nextInt();
+//        }
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         String[] input = br.readLine().split(" ");
 
-        //스트림을 이용해 String배열을 int배열로 전환
         arr = Stream.of(input).mapToInt(Integer::parseInt).toArray();
-
-        int M = Integer.parseInt(br.readLine());
-        String[] findInput = br.readLine().split(" ");
-        int[] find = Stream.of(findInput).mapToInt(Integer::parseInt).toArray();
-
-        StringBuilder sb = new StringBuilder();
         Arrays.sort(arr);
 
-        for(int i : find){
-            if(binarySearch(i,0,arr.length-1) != 0) sb.append("1").append("\n");
-            else sb.append("0").append("\n");
+        int m = Integer.parseInt(br.readLine());
+        String[] findInput = br.readLine().split(" ");
+        int[] findArr = Stream.of(findInput).mapToInt(Integer::parseInt).toArray();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<findArr.length; i++){
+            sb.append(getSolution(findArr[i],0,arr.length-1)).append("\n");
         }
         System.out.print(sb);
     }
