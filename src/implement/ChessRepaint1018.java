a44cdbff18090ccb62e45ca2e258edbc9816bf15
mikm7243 <mikm7243@naver.com>
2024-03-28 18:30:06 +0900

    feat : S4 수 찾기1920

--- src/implement/ChessRepaint1018.java
+++ src/implement/ChessRepaint1018.java
@@ -3,45 +3,45 @@ package implement;
 import java.util.Scanner;
 
 /*
-   문제를 읽고 고민해 보았지만 어떻게 풀어야할지 감이오지않아 답지를 보고 힌트를 얻고 풀었다. 현재까지 본 완전탐색중에선
-   가장 어려운거 같은데 이게 왜 실버4인지는 모르겠다...
-   다음주에 다시 풀어야 한다.
+   재풀이 :
+   Scanner의 nextInt는 개행문자를 삭제해주지 않아서 nextInt 이후에 nextLine을 받을경우 개행문자를 제거해줘야한다.
  */
 public class ChessRepaint1018 {
+    static String[] board;
 
-    static int getSolution(int startRow, int startCol, String[] board){
-        String[] pattern = {"BWBWBWBW","WBWBWBWB"};
-        int sol = 0;
+    private static int getSolution(int startRow, int startCol){
+        String[] ogBoard = {"BWBWBWBW","WBWBWBWB"};
+        int row, col = 0;
+        int count = 0;
 
         for(int i=0; i<8; i++){
-            int row = i + startRow;
+            row = startRow + i;
             for(int j=0; j<8; j++){
-                int col = j + startCol;
-                if(board[row].charAt(col) != pattern[row % 2].charAt(j)) sol++;
+                col = startCol + j;
+                if(board[row].charAt(col) != ogBoard[row % 2].charAt(j)) count++;
             }
         }
-        return Integer.min(sol,64-sol);
+        return Integer.min(count,64-count);
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int row = sc.nextInt();
         int col = sc.nextInt();
         sc.nextLine();
 
-        String[] board = new String[row];
-        for(int i=0; i<row; i++) {
+        board = new String[row];
+
+        for(int i=0; i<row; i++){
             board[i] = sc.nextLine();
         }
 
-        int prior = Integer.MAX_VALUE;
-        int min,compare = 0;
+        int sol = Integer.MAX_VALUE;
         for(int i=0; i<row-7; i++){
             for(int j=0; j<col-7; j++){
-                min = getSolution(i,j,board);
-                compare = Integer.min(prior,min);
-                prior = compare;
+                sol = Integer.min(sol,getSolution(i,j));
             }
         }
-        System.out.print(compare);
+        System.out.print(sol);
     }
 }
