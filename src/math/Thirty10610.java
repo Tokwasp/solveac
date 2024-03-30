f6895a55b11d5abb425710012e0f436f901ab2f5
mikm7243 <mikm7243@naver.com>
2024-03-30 15:04:58 +0900

    feat : S4 30 , 문자열 집합14425, 균형 잡힌 세계4949

---
+++ src/math/Thirty10610.java
@@ -0,0 +1,39 @@
+package math;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Thirty10610 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("");
+        boolean determine = false;
+        String st = " ";
+        StringBuilder sb = new StringBuilder();
+
+        //0이 있어야 30의 배수가 되므로 검사한다.
+        for(int i=0; i<input.length; i++){
+            st = input[i];
+            if(st.equals("0")) {
+                determine = true;
+                break;
+            }
+        }
+
+        //각자리 수의 합이 3의 배수여야 한다.
+        if(determine){
+            Integer[] arr = Stream.of(input).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
+            Arrays.sort(arr, Comparator.reverseOrder());
+
+            int count = 0;
+            for(int i:arr) count += i;
+            if(count % 3 == 0){
+                for(int i: arr) sb.append(i);
+            }
+            else sb.append(-1);
+        }
+        else sb.append(-1);
+        System.out.print(sb);
+    }
+}
---
+++ src/string/StringSet14425.java
@@ -0,0 +1,24 @@
+package string;
+
+import java.util.*;
+
+public class StringSet14425 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+
+        Set<String> nSet = new HashSet<>();
+
+        int count = 0;
+        for(int i=0; i<n; i++){
+            nSet.add(sc.nextLine());
+        }
+        for(int i=0; i<m; i++){
+            String input = sc.nextLine();
+            if(nSet.contains(input)) count++;
+        }
+        System.out.print(count);
+    }
+}
