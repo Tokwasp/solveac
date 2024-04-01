9183c3a3623aaa2be65199108b76564522b78de2
mikm7243 <mikm7243@naver.com>
2024-04-01 17:07:25 +0900

    feat : S3 퇴사14501, 타일링11726,  피보나치 수열1003

---
+++ src/math/FibonacciFuction1003.java
@@ -0,0 +1,26 @@
+package math;
+
+import java.util.Scanner;
+
+public class FibonacciFuction1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[] zeroDp = new int[41];
+        int[] oneDp = new int[41];
+        StringBuilder sb = new StringBuilder();
+
+        zeroDp[0] = 1; oneDp[1] = 1;
+        for(int i=0; i<t; i++){
+            int n = sc.nextInt();
+            for(int j=2; j<=n; j++){
+                if(zeroDp[j] == 0 && oneDp[j] == 0){
+                    zeroDp[j] = zeroDp[j-2] + zeroDp[j-1];
+                    oneDp[j] = oneDp[j-2] + oneDp[j-1];
+                }
+            }
+            sb.append(zeroDp[n]).append(" ").append(oneDp[n]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
