572b61195bb79edb8317a9256b480170a59c1ec6
mikm7243 <mikm7243@naver.com>
2024-04-01 22:27:10 +0900

    feat : S5 수들의합 1789

---
+++ src/greedy/NumberSum1789.java
@@ -0,0 +1,22 @@
+package greedy;
+
+import java.util.Scanner;
+
+public class NumberSum1789 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long s = sc.nextLong();
+
+        int count = 0;
+        long sum = 0;
+        for(int i=1; i<s; i++){
+            if(sum + i > s) break;
+            else {
+                sum += i;
+                count++;
+            }
+        }
+        if(s == 1) System.out.print("1");
+        else System.out.print(count);
+    }
+}
