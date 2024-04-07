f80e5dc7edd730e106279d4b7bcea958a07a7401
mikm7243 <mikm7243@naver.com>
2024-04-08 01:09:21 +0900

    refactor : S1 RGB거리1149
    2차원 배열 두개를 쓰지 않고 하나로 해결 되므로
    코드 리펙 토링

--- src/main/java/algorithm/math/RgbDistance1149.java
+++ src/main/java/algorithm/math/RgbDistance1149.java
@@ -7,27 +7,21 @@ public class RgbDistance1149 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[][] colorAndPrice = new int[n][3];
-        int[][] dp = new int[n][3];
-        for(int i=0; i<n; i++){
+        int[][] dp = new int[n+1][3];
+
+        for(int i=1; i<=n; i++){
             String[] input = br.readLine().split(" ");
 
-            colorAndPrice[i][0] = Integer.parseInt(input[0]);
-            colorAndPrice[i][1] = Integer.parseInt(input[1]);
-            colorAndPrice[i][2] = Integer.parseInt(input[2]);
+            int red = Integer.parseInt(input[0]);
+            int green = Integer.parseInt(input[1]);
+            int blue = Integer.parseInt(input[2]);
 
-            dp[0][0] = colorAndPrice[0][0];
-            dp[0][1] = colorAndPrice[0][1];
-            dp[0][2] = colorAndPrice[0][2];
-
-            if(i > 0){
-                dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + colorAndPrice[i][0];
-                dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + colorAndPrice[i][1];
-                dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1]) + colorAndPrice[i][2];
-            }
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + red;
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + green;
+            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1]) + blue;
         }
-        int min = Math.min(dp[n-1][0],dp[n-1][1]);
-        min = Math.min(min,dp[n-1][2]);
+        int min = Math.min(dp[n][0],dp[n][1]);
+        min = Math.min(min,dp[n][2]);
         System.out.println(min);
     }
 }
