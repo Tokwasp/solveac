aab7ca7f4ded87162c54ae30dd23927487afd828
mikm7243 <mikm7243@naver.com>
2024-05-08 12:23:52 +0900

    refactor : D3 Palindrome,MemoryRecovery
    펠린 드롬 메소드 두개 반복 되어 한개로 수정,
    메모리 복구 주석 추가

--- src/main/java/algorithm/swea/d3/Palindrome.java
+++ src/main/java/algorithm/swea/d3/Palindrome.java
@@ -2,7 +2,19 @@ package algorithm.swea.d3;
 
 import java.io.*;
 
+/*
+    문제 풀이 :
+    풀이 방법 : 구현
+
+    모든 행과, 모든 열에, 펠린 드롬수 만큼 문자를 가져와 뒤집어 비교 하여 찾는다.
+    행(가로)를 검사 한다고 하였을 떄,
+    각 행에서 펠린 드롬을 만들 수 있는 개수는 가로 길이 - 펠린 드롬 수 + 1 이다.
+
+    ex) 가로의 길이 : 8 , 길이가 4인 펠린 드롬을 만들 수 있는 경우의 수는 5 이다.
+ */
 public class Palindrome {
+    static int PalindromeCount = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -18,61 +30,52 @@ public class Palindrome {
                 }
             }
 
-            //가로체크
-            int widthPalindromeCount = widthPalindromeCheck(stArr, palindromeNum);
-            //세로체크
-            int heightPalindromeCount = heightPalindromeCheck(stArr, palindromeNum);
+            PalindromeCount = widthHeightPalindromeCheck(stArr, palindromeNum);
 
-            int PalindromeCount = widthPalindromeCount + heightPalindromeCount;
             String st = "#" + i + " " + PalindromeCount;
             sb.append(st).append("\n");
+            PalindromeCount = 0;
         }
         System.out.print(sb);
     }
 
-    static int widthPalindromeCheck(String[][] stArr, int palindromeNum){
+    static int widthHeightPalindromeCheck(String[][] stArr, int palindromeNum){
         int palindromeCount = 0;
         int repeat = stArr.length - palindromeNum + 1;
 
         for(int i=0; i<stArr.length; i++) {
-            int startNum = 0;
-            int endNum = palindromeNum - 1;
-            for(int j=0; j<repeat; j++) {
-                StringBuilder sb = new StringBuilder();
 
-                for (int k = startNum; k <= endNum; k++) {
-                    sb.append(stArr[i][k]);
-                }
-
-                String st = sb.toString();
-                String reverseString = sb.reverse().toString();
-                if (st.equals(reverseString)) palindromeCount++;
-                startNum++; endNum++;
-            }
-        }
-        return palindromeCount;
-    }
-
-    static int heightPalindromeCheck(String[][] stArr, int palindromeNum){
-        int palindromeCount = 0;
-        int repeat = stArr[0].length - palindromeNum + 1;
-
-        for(int i=0; i<stArr.length; i++) {
             int startNum = 0;
             int endNum = palindromeNum - 1;
 
             for(int j=0; j<repeat; j++) {
-                StringBuilder sb = new StringBuilder();
+
+                //행 검사 StringBuilder
+                StringBuilder rowSb = new StringBuilder();
+                //열 검사 StringBuilder
+                StringBuilder colSb = new StringBuilder();
 
                 for (int k = startNum; k <= endNum; k++) {
-                    sb.append(stArr[k][i]);
+                    rowSb.append(stArr[i][k]);
+                    colSb.append(stArr[k][i]);
                 }
 
-                String st = sb.toString();
-                String reverseString = sb.reverse().toString();
-                if (st.equals(reverseString)) palindromeCount++;
+                //문자로 만들고 StringBuilder.reverse()를 통해 문자를 뒤 집는다.
+
+                //행 검사
+                String rowSt = rowSb.toString();
+                String rowReverseString = rowSb.reverse().toString();
+
+                //열 검사
+                String colSt = colSb.toString();
+                String colReverseString = colSb.reverse().toString();
+
+                //뒤 집은 문자 열과 원래 문자 열이 같을 경우 펠린 드롬 이다.
+                if (rowSt.equals(rowReverseString)) palindromeCount++;
+                if (colSt.equals(colReverseString)) palindromeCount++;
                 startNum++; endNum++;
             }
+
         }
         return palindromeCount;
     }
