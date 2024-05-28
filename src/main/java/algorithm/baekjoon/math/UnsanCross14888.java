00490d53a72baa2acdcf227c3e993d7a649de1ce
mikm7243 <mikm7243@naver.com>
2024-05-28 13:16:15 +0900

    feat : S1 연산자 끼워넣기14888

---
+++ src/main/java/algorithm/baekjoon/math/UnsanCross14888.java
@@ -0,0 +1,56 @@
+package algorithm.baekjoon.math;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class UnsanCross14888 {
+    static int[] arr;
+    static int[] operator;
+
+    static int max = Integer.MIN_VALUE;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        operator = new int[4];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for(int i = 0; i < 4; i++){
+            int count = Integer.parseInt(st.nextToken());
+            operator[i] = count;
+        }
+
+        dfs(arr[0],1);
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+    static void dfs(int num, int index){
+        if(index == arr.length){
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            return;
+        }
+
+        for(int i = 0; i < 4; i++){
+            if(operator[i] > 0){
+
+                operator[i]--;
+
+                switch (i){
+                    case 0: dfs(num + arr[index], index + 1); break;
+                    case 1: dfs(num - arr[index], index + 1); break;
+                    case 2: dfs(num * arr[index], index + 1); break;
+                    case 3: dfs(num / arr[index], index + 1); break;
+                }
+
+                operator[i]++;
+            }
+        }
+    }
+}
