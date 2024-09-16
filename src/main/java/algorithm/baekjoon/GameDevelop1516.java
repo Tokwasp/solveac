172f05bc43658812762c3a0863ce9c64a924fafe
mikm7243 <mikm7243@naver.com>
2024-09-16 20:54:43 +0900

    feat : G3 게임 개발1516

---
+++ src/main/java/algorithm/baekjoon/GameDevelop1516.java
@@ -0,0 +1,67 @@
+package algorithm.baekjoon;
+
+import java.io.*;
+import java.util.*;
+import java.util.StringTokenizer;
+
+public class GameDevelop1516 {
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<ArrayList<Integer>> connectList;
+    static int[] indegree;
+    static int[] cost;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N + 1];
+        indegree = new int[N + 1];
+        connectList = new ArrayList<>();
+
+        for(int i = 0; i <= N; i++){
+            connectList.add(new ArrayList<>());
+        }
+
+        //입력 받기
+        for(int i = 1; i <= N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i] = Integer.parseInt(st.nextToken());
+
+            if(st.hasMoreTokens()){
+                while(true){
+                    int num = Integer.parseInt(st.nextToken());
+                    if(num == -1) break;
+                    else {
+                        connectList.get(num).add(i);
+                        indegree[i]++;
+                    }
+                }
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        int[] result = new int[N + 1];
+        for(int i = 1; i <= N; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ArrayList<Integer> nextNodeList = connectList.get(node);
+
+            for (int nextNode : nextNodeList) {
+                indegree[nextNode]--;
+                result[nextNode] = Math.max(result[nextNode], result[node] + cost[node]);
+
+                if (indegree[nextNode] == 0) {
+                    q.add(nextNode);
+                }
+            }
+        }
+
+        for(int i = 1; i <= N; i++){
+            sb.append(result[i] + cost[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
