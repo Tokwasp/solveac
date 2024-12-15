3c836d3e7bfbbdee4df691e889d7c2ece1a48b35
mikm7243 <mikm7243@naver.com>
2024-12-15 23:53:57 +0900

    feat : G5 contact

---
+++ src/main/java/algorithm/baekjoon/string/Contact.java
@@ -0,0 +1,18 @@
+package algorithm.baekjoon.string;
+
+import java.util.Scanner;
+
+public class Contact {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String vega = "(100+1+|01)+";
+        int t = scan.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            String spread = scan.next();
+            if(spread.matches(vega)) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
