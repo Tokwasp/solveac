06ce34a3d4d33b059c81827c9594ef632146ce0c
Tokwasp <mikm7243@naver.com>
2025-08-31 01:58:06 +0900

    feat: 프로그래머스 Level2 줄서는 방법

---
+++ src/main/java/algorithm/programmers/math/LineUp.java
@@ -0,0 +1,31 @@
+package algorithm.programmers.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LineUp {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> numbers = new ArrayList<>();
+        long[] factorial = new long[n];
+
+        k = k - 1;
+
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+
+        factorial[0] = 1;
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int idx = (int) (k / factorial[n - 1 - i]);
+            answer[i] = numbers.get(idx);
+            numbers.remove(idx);
+            k %= factorial[n - 1 - i];
+        }
+        return answer;
+    }
+}
