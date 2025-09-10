a203de76546cbf018f76be8c128857465205c8f9
Tokwasp <mikm7243@naver.com>
2025-09-10 18:15:40 +0900

    feat: 프로그래머스 Level2 숫자 변환하기

--- src/main/java/algorithm/programmers/dp/ConvertNumber.java
+++ src/main/java/algorithm/programmers/dp/ConvertNumber.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class ConvertNumber {
+
+    // dp 풀이
     public int solution(int x, int y, int n) {
         int[] dp = new int[y + 1];
 
@@ -38,5 +40,43 @@ public class ConvertNumber {
             }
         }
         return dp[y];
+    }
+
+    // 2번째 풀이 bfs
+    public int solution2(int x, int y, int n) {
+        boolean[] visited = new boolean[y + 1];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, 0});
+        visited[x] = true;
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int num = poll[0];
+            int dist = poll[1];
+
+            if (num == y) {
+                return dist;
+            }
+
+            int nextPlus = num + n;
+            int nextProductTwo = num * 2;
+            int nextProductThree = num * 3;
+
+            if (nextPlus <= y && !visited[nextPlus]) {
+                visited[nextPlus] = true;
+                queue.add(new int[]{nextPlus, dist + 1});
+            }
+
+            if (nextProductTwo <= y && !visited[nextProductTwo]) {
+                visited[nextProductTwo] = true;
+                queue.add(new int[]{nextProductTwo, dist + 1});
+            }
+
+            if (nextProductThree <= y && !visited[nextProductThree]) {
+                visited[nextProductThree] = true;
+                queue.add(new int[]{nextProductThree, dist + 1});
+            }
+        }
+        return -1;
     }
 }
