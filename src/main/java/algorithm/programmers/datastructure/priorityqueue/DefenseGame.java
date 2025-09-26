ce54dc15dbf4e66508ad35c92ff2a0617ac09f6f
Tokwasp <mikm7243@naver.com>
2025-09-26 23:54:22 +0900

    feat: 프로그래머스 Level2 디펜스게임 재풀이

--- src/main/java/algorithm/programmers/datastructure/priorityqueue/DefenseGame.java
+++ src/main/java/algorithm/programmers/datastructure/priorityqueue/DefenseGame.java
@@ -21,4 +21,25 @@ public class DefenseGame {
         }
         return enemies.length;
     }
+
+    // 재풀이
+    public int solution2(int life, int shildCount, int[] enemies) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+
+        for(int i = 0 ; i < enemies.length; i++){
+            int enemy = enemies[i];
+            life -= enemy;
+            pq.add(enemy);
+
+            if(life < 0 && shildCount > 0){
+                life += pq.poll();
+                shildCount--;
+            }
+
+            if(life < 0){
+                return i;
+            }
+        }
+        return enemies.length;
+    }
 }
