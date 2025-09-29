a7a939c269ddcbb201f2ecb2ddc70e655872f135
Tokwasp <mikm7243@naver.com>
2025-09-29 19:00:02 +0900

    feat: 프로그래머스 Level2 튜플

--- src/main/java/algorithm/programmers/string/Tuple.java
+++ src/main/java/algorithm/programmers/string/Tuple.java
@@ -46,4 +46,40 @@ public class Tuple {
         }
         return result;
     }
+
+    public int[] solution2(String input) {
+        String[] numbers = input.substring(2, input.length() - 2)
+                .replace("},{", "-")
+                .split("-");
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        for(int i = 0; i < numbers.length; i++){
+            String[] numberInput = numbers[i].split(",");
+
+            list.add(new ArrayList<>());
+
+            for(String number: numberInput){
+                list.get(i).add(Integer.parseInt(number));
+            }
+        }
+
+        list.sort((a,b) -> a.size() - b.size());
+
+        Set<Integer> set = new HashSet<>();
+        int[] result = new int[list.size()];
+        int resultIndex = 0;
+
+        for(int i = 0; i < list.size(); i++){
+            List<Integer> numberList = list.get(i);
+
+            for(int num :numberList){
+                if(!set.contains(num)){
+                    set.add(num);
+                    result[resultIndex++] = num;
+                }
+            }
+        }
+        return result;
+    }
 }
